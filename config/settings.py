4658de1ad68a7d1e9448d65848aed1784b4c7866
Iman Jahed <101338494+ImanJahed@users.noreply.github.com>
2023-11-25 21:20:33 +0330

    added pages app and set base template

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd Party Apps
+    
+    # Local Apps
+    'pages.apps.PagesConfig',
+    
+    
 ]
 
 MIDDLEWARE = [
